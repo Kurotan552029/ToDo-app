d06db59e5dced4ac0d5bd74feea3deb89d4f9061
山本　輝 <kuro2525te@ezweb.ne.jp>
2025-08-21 17:23:14 +0900

    絶対パスに修正

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,12 @@ self.addEventListener("install", (e) => {
     e.waitUntil(
         caches.open("todo-cache").then((cache) => {
             return cache.addAll([
-                "./index.html",
-                "./style.css",
-                "./script.js",
-                "./manifest.json",
-                "./icon-192.png",
-                "./icon-512.png"
+                "/index.html",
+                "/style.css",
+                "/script.js",
+                "/manifest.json",
+                "/icon-192.png",
+                "/icon-512.png"
             ]);
         })
     );
